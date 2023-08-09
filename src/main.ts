acdb108d0d9b3abba19a1a1ea733f17f12c1bff9
pailodze <mikheilizee@gmail.com>
2023-08-09 19:20:03 +0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.BACKEND_PORT);
+  await app.listen(+process.env.BACKEND_PORT);
 }
 bootstrap();
