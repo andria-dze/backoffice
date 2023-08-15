b7eb2fe4db0101ff3a27e9ac58a46a24ca3ecc6e
andria-dze <nika.andriadze@grover.com>
2023-08-15 16:34:08 +0200

    feat: enableCors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { CONFIG } from './infra/config/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(Number(CONFIG.PARAMS.APP.BACKEND_PORT));
 }
 bootstrap();
